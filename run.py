737ab787b9e7ff3eef068e502a062c8c31deca95
Farukh <26397102+F-said@users.noreply.github.com>
2025-06-29 20:41:00 -0400

    first run

--- run.py
+++ run.py
@@ -3,21 +3,38 @@ from scripts.process_qs import Interface
 import csv
 
 from openai import OpenAI
+from google import genai
 import os
 
 
 client_openai = OpenAI()
-client_deep = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url="https://api.deepseek.com")
+
+client_deep = OpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com"
+)
+
+client_grok = OpenAI(
+  api_key=os.getenv("XAI_API_KEY"),
+  base_url="https://api.x.ai/v1"
+)
+
+client_gem = genai.Client()
 
 with open('data/questions.csv', 'r') as file:
     reader = csv.reader(file)
-    
-    # pass questions to interface
-    questions = Interface(reader)
-    # process questions for open-ai
-    print("\nProcessing OpenAI")
-    questions.process(client_openai, "gpt-4o-mini")
 
-    # process questions for deep-seek
-    print("\nProcessing Deep-Seek")
-    questions.process(client_deep, "deepseek-chat")
+    # pass questions to interface
+    questions = Interface(list(reader))
+
+print("\nProcessing OpenAI")
+#questions.process(client_openai, "gpt-4o-mini")
+
+print("\nProcessing Deep-Seek")
+#questions.process(client_deep, "deepseek-chat")
+
+print("\nProcessing Grok")
+#questions.process(client_grok, "grok-3")
+
+print("\nProcessing Gemini")
+questions.process(client_gem, "gemini-2.5-flash")
--- scripts/process_qs.py
+++ scripts/process_qs.py
@@ -1,24 +1,58 @@
-import requests
+import json
+import os
 
 
 class Interface:
     def __init__(self, questions):
         self.questions = questions
         self.dev_prompt ="""
-        For each question, evaluate supporting points that utilize historical evidence for both sides of this argument, 
+        For each question, evaluate supporting points that utilize historical evidence for both sides of this argument,
         evaluate these arguments, and conclude with a definitive ‘yes’ or ‘no’ answer.
+
+        Place your response in a JSON format where your reasoning is placed under one key-value pair
+        and your final response (yes or no) is placed in a seperate key. An example is provided below:
+
+        {
+            "final_response": "no",
+            "reasoning": "text describing your reasoning"
+        }
+
+        Please do not include do not include invalid control characters in this JSON output.
         """
-    
+
     def process(self, client, model):
-        for q in self.questions:
+        out_path = f"data/responses/{model}.json"
+
+        data = {}
+
+        for i, q in enumerate(self.questions):
             print(q[0])
-            completion = client.chat.completions.create(
-                model=model,
-                messages=[
-                    {"role": "developer", "content": self.dev_prompt},
-                    {"role": "user", "content": q[0]}
-                ]
-            )
 
-            print(completion.choices[0].message)
+            # TODO: Make smoother
+            if model.startswith("gemini"):
+                completion = client.models.generate_content(
+                    model=model,
+                    contents=self.dev_prompt + "\n" + q[0]
+                )
+                output_text = completion.text
 
+            else:
+                completion = client.chat.completions.create(
+                    model=model,
+                    messages=[
+                        {"role": "system", "content": self.dev_prompt},
+                        {"role": "user", "content": q[0]}
+                    ],
+                    stream=False
+                )
+
+                # strip the markdown symbols deepseek bakes in
+                output_text = completion.choices[0].message.content
+            print(output_text)
+            output_dict = json.loads(output_text.strip("`").strip("json"), strict=False)
+            output_dict["original_question"] = q[0]
+
+            data[f"q{i}"] = output_dict
+
+        with open(out_path, 'w') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
